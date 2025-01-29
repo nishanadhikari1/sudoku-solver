a5a79d99617487268d8d52a7247ba7a34783c048
nishanadhikari1 <adhikarin537@gmail.com>
2025-01-29 13:38:16 +0545

    added basic template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className= "bg-blue-500 text-white text-center p-10">
+      Hello, World!
+    </div>
+  )
+}
+
+export default App
